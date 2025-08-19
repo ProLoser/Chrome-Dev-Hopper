316183c7bffa9c977663e544c74f4c5c4537b8d8
Dean Sofer <Dean.Sofer@sony.com>
2025-08-19 10:54:38 -0700

    Fixed tab switching

--- background.ts
+++ background.ts
@@ -14,40 +14,59 @@ export {}
 const shouldShowMap = new Map<number, boolean>()
 const storage = new Storage()
 
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.url) {
-      const projects = (await storage.get("projects")) || []
-      const shouldShow = projects?.find?.((project) =>
-        project.environments?.some((environment) =>
-          new RegExp(environment.regex).test(tab.url)
-        )
+const onUpdate = async (tabId, tab) => {
+  if (tab.url) {
+    const projects = (await storage.get("projects")) || []
+    const shouldShow = projects?.find?.((project) =>
+      project.environments?.some((environment) =>
+        new RegExp(environment.regex).test(tab.url)
       )
+    )
 
-      shouldShowMap.set(tabId, !!shouldShow)
+    shouldShowMap.set(tabId, !!shouldShow)
 
-      if (!!shouldShow) {
-        // chrome.action.setBadgeBackgroundColor({ color: "#4CAF50" })
-        chrome.action.setBadgeText({ text: `${shouldShow.environments.length}` })
-        // chrome.action.setTitle({ title: "Dev Hopper Active" })
-        // chrome.action.setBadgeTextColor({ color: "#FF0000" })
-        chrome.action.setPopup({ popup })
-        chrome.action.setIcon({ tabId, path: {
-          "16": icon16green,
-          "32": icon32green,
-          "48": icon64green,
-          "128": icon128green
-        } })
+    if (!!shouldShow) {
+      // chrome.action.setBadgeBackgroundColor({ color: "#4CAF50" })
+      // chrome.action.setTitle({ title: "Dev Hopper Active" })
+      // chrome.action.setBadgeTextColor({ color: "#FF0000" })
+      chrome.action.setBadgeText({ tabId, text: `${shouldShow.environments.length}` })
+      chrome.action.setPopup({ tabId, popup })
+      chrome.action.setIcon({ tabId, path: {
+        "16": icon16green,
+        "32": icon32green,
+        "48": icon64green,
+        "128": icon128green
+      } })
 
-      } else {
-        chrome.action.setPopup({ popup: '' })
-        chrome.action.setBadgeText({ text: '' })
-        chrome.action.setIcon({ tabId, path: {
-          "16": icon16blue,
-          "32": icon32blue,
-          "48": icon64blue,
-          "128": icon128blue
-        } })
-      }
+    } else {
+      chrome.action.setPopup({ tabId, popup: '' })
+      chrome.action.setBadgeText({ tabId, text: '' })
+      chrome.action.setIcon({ tabId, path: {
+        "16": icon16blue,
+        "32": icon32blue,
+        "48": icon64blue,
+        "128": icon128blue
+      } })
+    }
+  } else {
+    // chrome.action.setPopup({ tabId, popup: '' })
+    // chrome.action.setBadgeText({ tabId, text: '' })
+    // chrome.action.setIcon({ tabId, path: {
+    //   "16": icon16blue,
+    //   "32": icon32blue,
+    //   "48": icon64blue,
+    //   "128": icon128blue
+    // } })
+  }
+}
+
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  const tab = await chrome.tabs.get(activeInfo.tabId)
+  await onUpdate(tab.id, tab)
+})
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete") {
+    await onUpdate(tabId, tab)
   }
 })
 
